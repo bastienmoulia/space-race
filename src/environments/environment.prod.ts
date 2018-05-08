3d5cdbf5fa8bd06bad5980f5295aa0ade14b5b87
Bastien <bmoulia@gmail.com>
2018-05-08 18:13:47 +0200

    fix environment for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBg1Bcc-S_MYFqYCosINDSIFL2ET6OGRJ8',
+    authDomain: 'space-race-3ea43.firebaseapp.com',
+    databaseURL: 'https://space-race-3ea43.firebaseio.com',
+    projectId: 'space-race-3ea43',
+    storageBucket: 'space-race-3ea43.appspot.com',
+    messagingSenderId: '878756379699'
+  }
 };
